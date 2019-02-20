b114795ac4251c10c341c22d5dd04ddeafb6acd1
Huzhuwei1 <1806899153@qq.com>
2019-02-20 18:07:49 +0800

    Initial commit

---
+++ tinkerdemo/src/main/java/com/hzw/tinkerdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hzw.tinkerdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("TApp","------------------");
+    }
+}
